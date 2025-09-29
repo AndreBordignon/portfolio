26110dbdec8b5d0fb8106ccb241fe966cc3122c2
Andre Bordignon <andrebordignonn@gmail.com>
2025-09-29 15:41:27 -0300

    add newsletter substack

--- src/app/components/NewsletterForm/index.tsx
+++ src/app/components/NewsletterForm/index.tsx
@@ -4,6 +4,6 @@ export default function NewsletterForm() {
 
 
   return (
-    <iframe className="flex flex-1 w-full bg-transparent" src="https://andrebordignon.substack.com/embed" width="480" height="370" style={{ border: "1px solid #EEE", background: "transparent" }} frameBorder="0" scrolling="no"></iframe>
+    <iframe className="flex flex-1 w-full bg-transparent" src="https://andrebordignon.substack.com/embed" width="480" height="300" style={{ border: "1px solid #EEE", background: "transparent" }} frameBorder="0" scrolling="no"></iframe>
   );
 }
